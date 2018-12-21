97641204fe82bc26f1a5edcfbf33d3d35b82e9c1
Patrick Biffle <pbiffle@gmail.com>
2018-12-21 12:46:36 -0700

    initial commit, map working

---
+++ src/components/Map.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id='map'>
+
+  </div>
+</template>
+
+<script>
+import L from 'leaflet'
+
+export default {
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: 3
+    }
+  },
+  components: {
+  },
+  methods: {
+    initMap () {
+      var map = L.map('map').setView([36, 116], 4)
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
+    }
+  },
+  mounted () {
+    this.initMap()
+  }
+}
+</script>
+
+<style>
+  #map {
+    display: flex;
+    flex-flow: column nowrap;
+    height: 500px;
+    width: 700px;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map1 />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Map1 from '@/components/Map.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Map1
   }
 }
 </script>
+
+<style>
+</style>
